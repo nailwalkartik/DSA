7267e4a7bf6d14530e0713c512f6ae659f6f43f4
nailwalkartik <kartik18nailwal@gmail.com>
2022-06-22 10:42:22 +0530

    Added new solutions of String Manipulation genre question: Find Substring Index

---
+++ src/FindSubstringIndex.java
@@ -0,0 +1,35 @@
+public class FindSubstringIndex {
+    public static void main(String[] args) {
+        String s = "1231231234";
+        String x = "41";
+
+        int xInd = 0;
+        int xLen = x.length();
+        int foundInd = 0;
+
+        for (int i = 0; i < s.length(); i ++) {
+            int k = i;
+            for (int j = 0; j < xLen; j ++) {
+                if (xInd == xLen) {
+                    break;
+                }
+                if (k > s.length() - 1) {
+                    break;
+                }
+                if (s.charAt(k) == x.charAt(j)) {
+                    foundInd = i;
+                    k ++;
+                    xInd ++;
+                } else {
+                    xInd = 0;
+                    break;
+                }
+            }
+        }
+        if (xInd > 0 && xInd == xLen) {
+            System.out.println(foundInd);
+        } else {
+            System.out.println("-1");
+        }
+    }
+}
